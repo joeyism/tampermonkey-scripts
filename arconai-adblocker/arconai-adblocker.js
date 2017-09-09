4cb4d1a642f4adf66a731706cdbf93b36f7c6b73
joeyism <sham.joey@gmail.com>
2017-09-09 16:16:23 -0400

    Updated arconai-adblocker for a wider range of arconai sites

--- arconai-adblocker/arconai-adblocker.js
+++ arconai-adblocker/arconai-adblocker.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       joeyism
-// @match        http://arconaitv.me/*
+// @include      http*://*.arconaitv.*/*
 // @grant        none
 // @run-at document-start
 // ==/UserScript==
